d006e79f2401537673ef09da55c0629cd56e2b3b
Kevin <vuibinhyen2003@gmail.com>
2022-09-10 11:38:00 +0700

    day 2

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,49 @@
+import { Container } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
 import { Player } from "video-react";
 import HomeTemplate from "../template/HomeTemplate";
 export default function Home() {
   return (
-    <HomeTemplate>
-      <Carousel className="home-carousel">
-        <Carousel.Item>
-          <Player
-            playsInline
-            autoPlay={true}
-            poster="/assets/518.jpg"
-            src="/assets/01.MP4"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <HomeTemplate themeClasses="dark-theme">
+      <Container>
+        <Carousel controls={false} indicators={false} className="home-carousel">
+          <Carousel.Item interval={8000}>
+            <div className="home-info">
+              <div className="home-details">
+                <div className="home-detail">Creative Agency</div>
+                <div className="home-detail">Brand Agency</div>
+                <div className="home-detail">Brand Strategy</div>
+                <div className="home-detail">Brand Identity</div>
+                <div className="home-detail">Visual Unity Solution</div>
+              </div>
+              <div className="home-intro">
+                Hello!
+                <br />
+                UOVO is a agency based in Hanoi in 2018.
+                <br /> We aim for unfolding your brand story, mapping great
+                brand strategies and letting them to win customer’s mind.
+                <br />
+                Your story is what inspired us!
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item interval={8000}>
+            <div className="d-flex justify-content-center">
+              <Player
+                src="/assets/01.MP4"
+                fluid={true}
+                autoPlay={true}
+              ></Player>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item interval={8000}>
+            <div className="d-flex justify-content-center mt-5">
+              <img src="/assets/logo3.png" alt="logo 3" />
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
     </HomeTemplate>
   );
 }
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,7 +1,59 @@
 import Image from "next/image";
 import React from "react";
 import HomeTemplate from "../../template/HomeTemplate";
+
+const dataNameImageJPG = [
+  "518",
+  "And VietNam",
+  "Bazan",
+  "Casakid",
+  "Casamia",
+  "CICT",
+  "Cơm tấm Nam Kỳ",
+  "Davicon",
+  "deVUI",
+  "Dobe",
+  "Enzym Nutri",
+  "Euro milk",
+  "Hicon 3",
+  "HikVision",
+  "Hilook",
+  "ITC",
+  "Joy 3",
+  "Joy plus",
+  "King edu",
+  "KK Lịch",
+  "KK Sapa",
+  "Levirtu",
+  "Lovebook 3",
+  "Lovebook",
+  "Mekong capital",
+  "NAP",
+  "NGN",
+  "Persident cusise",
+  "Phở Từ",
+  "SUNNY",
+  "Vietlesson",
+  "WOMO",
+  "Yên Tử",
+  "Sữa chua trân châu hạ long"
+];
+const dataNameImagePNG = ["Fami", "FC racing", "z125"];
 export default function index() {
+  const renderImage = (listImage, format) => {
+    const formatImg = format ? "jpg" : "png";
+    return listImage.map((item, index) => {
+      return (
+        <li key={index} className="work-item">
+          <p className="work-item__name">{item}</p>
+          <div className="hide showImg">
+            <img src={`/assets/images/${item}.${formatImg}`} />
+          </div>
+        </li>
+      );
+    });
+  };
+
   return (
     <HomeTemplate themeClasses="light-theme">
       <div className="page work">
@@ -9,37 +61,18 @@ export default function index() {
           <div className="page-container">
             <div className="page-content">
               <ul className="list-work">
-                <li className="work-item">
-                  <p className="work-item__name">WOMO</p>
-                  <div className="hide showImg">
-                    <Image
-                      src="/assets/images/WOMO.jpg"
-                      width="500px"
-                      height="250px"
-                    ></Image>
-                  </div>
-                </li>
-                <li className="work-item">
-                  <p className="work-item__name">Bazan</p>
-                  <div className="hide showImg">
-                    <Image
-                      src="/assets/images/Bazan.jpg"
-                      width="500px"
-                      height="250px"
-                    ></Image>
-                  </div>
-                </li>
-                <li className="work-item">
-                  <p className="work-item__name">518</p>
-                  <div className="hide showImg">
-                    <Image
-                      src="/assets/images/518.jpg"
-                      width="500px"
-                      height="250px"
-                    ></Image>
-                  </div>
-                </li>
+                {renderImage(dataNameImageJPG, true)}
+                {renderImage(dataNameImagePNG, false)}
               </ul>
+              <div className="home-info">
+                <div className="home-details">
+                  <div className="home-detail">Creative Agency</div>
+                  <div className="home-detail">Brand Agency</div>
+                  <div className="home-detail">Brand Strategy</div>
+                  <div className="home-detail">Brand Identity</div>
+                  <div className="home-detail">Visual Unity Solution</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
